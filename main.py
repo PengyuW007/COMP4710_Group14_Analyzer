e6cfac3db61560db903b544fd66c410faf6b924b
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-11-21 22:24:59 -0600

    CHENZHIYI

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print("COMP 4710 Group 14 Analyzer.\n")
+
+print("CHENZHI YI")
+t = 1 + 2 == 4
+
+if t:
+    print("Yes\n")
+else:
+    print("False\n")
+
+list = ['red', 'green', 'blue', 'yellow', 'white', 'black']
+
+print(list[-1])
+print(list[-2])
+print(list[-3])
