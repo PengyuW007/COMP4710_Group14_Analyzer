018beeac00318e05889f7d03acd292c6c0387d83
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-11-29 23:36:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import xlrd  # you should install xlrd package, resolvent: pip install xlrd
+import xlrd  # you should install xlrd package, resolvent: pip install xlrd/ pip install openpyxl
 
 '''
 Read
@@ -11,7 +11,7 @@ file_loc = "data.xlsx"  # copy the root of repository's path, if you have error"
 data = xlrd.open_workbook(file_loc)  # all the data inside excel file
 # sheet
 sheet = data.sheet_by_index(0)  # read sheet one
-numRows = sheet.nrows - 1
+numRows = sheet.nrows
 print("number of rows: ", numRows, '\n')
 
 # print("COMP 4710 Group 14 Analyzer.\n")
