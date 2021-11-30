e51775da83cd4192f58be0b4f548efd352765498
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-11-29 22:53:49 -0600

    Initialize data from excel

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import numpy as np
+import xlrd  # you should install xlrd package
+
+'''
+Read
+'''
+# path
+file_loc = "data.xlsx"  # copy the root of repository's path
+data = xlrd.open_workbook(file_loc)  # all the data inside excel file
+# sheet
+sheet = data.sheet_by_index(0)  # read sheet one
+numRows = sheet.nrows - 1
+# print("number of rows: ", numRows, '\n')
+
 print("COMP 4710 Group 14 Analyzer.\n")
