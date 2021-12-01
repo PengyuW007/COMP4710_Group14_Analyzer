524deb3b4616ac357ecb4a6f29bcd85922b28a7d
mayue1998 <mayue1998@icloud.com>
2021-11-30 18:28:10 -0600

    test
    just for testing

--- main.py
+++ main.py
@@ -14,4 +14,20 @@ sheet = data.sheet_by_index(0)  # read sheet one
 numRows = sheet.nrows
 print("number of rows: ", numRows, '\n')
 
+'''yue ma tests, '''
+testFile = pd.read_excel("data.xlsx")
+
+
+def testRemove(count):
+    if count != 0:
+        print("The input value is not zero, please return and check!")
+    else:
+        for i in testFile["COV_GDR"]:
+            if i == 9:
+                count += 1
+
+
 # print("COMP 4710 Group 14 Analyzer.\n")
+
+if __name__ == '__main__':
+    testRemove(0)
